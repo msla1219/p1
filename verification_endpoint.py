f87edbacf50c753da454614b0ee15537d7797943
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-07-18 14:39:53 +0900

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -14,27 +14,23 @@ def verify():
     content = request.get_json(silent=True)
 
     try:
-        '''
+
         if content['payload']['platform'] == 'Ethereum':
             eth_sk = content['sig']
             eth_pk = content['payload']['pk']
 
             payload = json.dumps(content['payload'])
             eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-            eth_sig_obj = eth_account.Account.recover_message(eth_encoded_msg, eth_sk)
-
-            print("payload:", payload)
-            print("eth_encoded_msg:", eth_encoded_msg)
-            print("eth_sig_obj:", eth_sig_obj)
+            recovered_pk = eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sk)
 
             # Check if signature is valid
-            if payload.strip() == eth_sig_obj:
+            if recovered_pk == eth_pk:
                 result = True
             else:
                 result = False
 
             return jsonify(result)
-        '''
+
         if content['payload']['platform'] == 'Algorand':
             algo_sig = content['sig']
             algo_pk = content['payload']['pk']
