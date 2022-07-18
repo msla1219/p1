923738ab5772a99565fd4ae7454dc461a0f1ab91
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-07-18 14:04:18 +0900

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -12,36 +12,38 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET', 'POST'])
 def verify():
     content = request.get_json(silent=True)
-    
+
     try:
         if content['payload']['platform'] == 'Ethereum':
-
             eth_sk = content['sig']
             eth_pk = content['payload']['pk']
 
             payload = json.dumps(content['payload'])
             eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-            eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg, eth_sk)
+            eth_sig_obj = eth_account.Account.recover_message(eth_encoded_msg, eth_pk)
 
             # Check if signature is valid
-            result = eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj.signature.hex()) == eth_pk
+            if eth_sig_obj.signature.hex() == eth_sk:
+                result = True
+            else:
+                result = False
+
             return jsonify(result)
 
         if content['payload']['platform'] == 'Algorand':
             payload = json.dumps(content['payload'])
 
-
             algo_sk = content['sig']
             algo_pk = content['payload']['pk']
             algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'), algo_sk)
 
-            result = algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,algo_pk)
+            result = algosdk.util.verify_bytes(payload.encode('utf-8'), algo_sig_str, algo_pk)
             return jsonify(result)
     except Exception as e:
         import traceback
         print(traceback.format_exc())
         print(e)
 
-        
+
 if __name__ == '__main__':
     app.run(port='5002')
